dd2136cf447b63a551582c653f11f5908b4f6b0f
hyveblockchain-dev <hyveblockchain@gmail.com>
2026-01-14 15:56:56 -0800

    Update sanity.config.js

--- studio2/sanity.config.js
+++ studio2/sanity.config.js
@@ -1,14 +1,21 @@
 import { defineConfig } from 'sanity'
 import { deskTool } from 'sanity/desk'
 
+const projectId = process.env.SANITY_STUDIO_PROJECT_ID || ''
+const dataset = process.env.SANITY_STUDIO_DATASET || ''
+
+if (!projectId || !dataset) {
+  // This makes the build fail with a CLEAR message instead of Rollup garbage
+  throw new Error(
+    'Missing SANITY_STUDIO_PROJECT_ID or SANITY_STUDIO_DATASET environment variables.'
+  )
+}
+
 export default defineConfig({
   name: 'default',
   title: 'HyveConnect',
-
-  projectId: process.env.SANITY_STUDIO_PROJECT_ID!,
-  dataset: process.env.SANITY_STUDIO_DATASET!,
-
+  projectId,
+  dataset,
   plugins: [deskTool()],
-
   basePath: '/',
 })
