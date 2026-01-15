df5066b68b4abeac376068576f1e24fac7a519cb
hyveblockchain-dev <hyveblockchain@gmail.com>
2026-01-14 16:28:39 -0800

    Update index.js

--- studio2/schemas/index.js
+++ studio2/schemas/index.js
@@ -3,6 +3,4 @@ import tweet from './tweetSchema'
 import user from './userSchema'
 
 export const schemaTypes = [post, tweet, user]
-
-// ✅ Provide default export too (Rollup expects this if you import default)
 export default schemaTypes
