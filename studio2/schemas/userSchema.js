e2e9b7b9d7ec422af99900b2c779d9e1ed36c485
hyveblockchain-dev <hyveblockchain@gmail.com>
2026-01-14 17:41:49 -0800

    Update userSchema.js

--- studio2/schemas/userSchema.js
+++ studio2/schemas/userSchema.js
@@ -1,34 +1,46 @@
 export default {
-  name: "user",
-  title: "User",
-  type: "document",
+  name: 'user',
+  title: 'User',
+  type: 'document',
   fields: [
     {
-      name: "walletAddress",
-      title: "Wallet Address",
-      type: "string",
+      name: 'walletAddress',
+      title: 'Wallet Address',
+      type: 'string',
       readOnly: true,
     },
     {
-      name: "name",
-      title: "Name",
-      type: "string",
+      name: 'name',
+      title: 'Name',
+      type: 'string',
     },
     {
-      name: "profileImage",
-      title: "Profile Image",
-      type: "image",
+      name: 'profileImage',
+      title: 'Profile Image',
+      type: 'image',
       options: {
         hotspot: true,
       },
     },
     {
-      name: "coverImage",
-      title: "Cover Image",
-      type: "image",
+      name: 'coverImage',
+      title: 'Cover Image',
+      type: 'image',
       options: {
         hotspot: true,
       },
+    },
+    {
+      name: 'isProfileImageNFT',
+      title: 'Is Profile Image NFT',
+      type: 'boolean',
+      initialValue: false,
+    },
+    {
+      name: 'createdAt',
+      title: 'Created At',
+      type: 'datetime',
+      readOnly: true,
     },
   ],
-};
+}
