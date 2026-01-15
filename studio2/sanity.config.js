7e782647289a4d326eb11a6ac0f5152a27e992c2
hyveblockchain-dev <hyveblockchain@gmail.com>
2026-01-14 16:09:13 -0800

    Update sanity.config.js

--- studio2/sanity.config.js
+++ studio2/sanity.config.js
@@ -1,19 +1,19 @@
 import {defineConfig} from 'sanity'
 import {deskTool} from 'sanity/desk'
 
-// 👇 This path might be different in your repo.
-// If this import errors, I’ll tell you the exact correct path after you check step 3.
-import {schemaTypes} from './schemaTypes'
+// 👇 THIS IS THE CORRECT PATH BASED ON YOUR REPO
+import schemas from './schemas'
 
 export default defineConfig({
   name: 'default',
   title: 'HyveConnect',
+
   projectId: process.env.SANITY_STUDIO_PROJECT_ID || 'khg0m9lx',
   dataset: process.env.SANITY_STUDIO_DATASET || 'production',
 
   plugins: [deskTool()],
 
   schema: {
-    types: schemaTypes,
+    types: schemas,
   },
 })
