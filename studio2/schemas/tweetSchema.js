c8e8a1d9a9e5eaa73c23dfbcb0978657fc93c2f8
hyveblockchain-dev <hyveblockchain@gmail.com>
2026-01-14 17:42:40 -0800

    Update tweetSchema.js

--- studio2/schemas/tweetSchema.js
+++ studio2/schemas/tweetSchema.js
@@ -1,25 +1,19 @@
 export default {
-  name: "tweets",
-  title: "Tweet",
-  type: "document",
+  name: 'tweets',
+  title: 'Tweets',
+  type: 'document',
   fields: [
     {
-      name: "tweet",
-      title: "Tweet",
-      type: "string",
-      validation: (Rule) => Rule.required(),
+      name: 'text',
+      title: 'Text',
+      type: 'string',
+      validation: Rule => Rule.required(),
     },
     {
-      name: "author",
-      title: "Author",
-      type: "reference",
-      to: [{ type: "user" }], // ✅ FIXED (was "users")
-    },
-    {
-      name: "createdAt",
-      title: "Created At",
-      type: "datetime",
-      readOnly: true,
+      name: 'author',
+      title: 'Author',
+      type: 'reference',
+      to: [{ type: 'user' }], // 🔴 MUST be 'user'
     },
   ],
-};
+}
